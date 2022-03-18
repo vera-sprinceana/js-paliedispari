4f123e8eca289b77fbae34f85e81f33f2eb00b2a
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-18 18:56:57 +0100

    pari-dispari 2

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -26,8 +26,8 @@ function getRandomInt(min, max) {
     }
 } console.log(numeroPariDispari(somma))
 
-if(numeroPariDispari(pariDispari)=="pari"){
-    document.getElementById("numero").innerHTML=`${pariDispari}L'utente ha vinto`;
+if(pariDispari=="pari"){
+    document.getElementById("numero").innerHTML=`${pariDispari}  L'utente ha vinto`;
 }else{
-    document.getElementById("numero").innerHTML=` L'utente ha perso`;
+    document.getElementById("numero").innerHTML=`${pariDispari}   L'utente ha perso`;
 }
