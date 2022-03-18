708dcfcb6945bf92e5f7d1207b3c604b25835dc8
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-18 18:46:02 +0100

    Pari e Dispari

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -3,3 +3,31 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
+let pariDispari=prompt("Vuoi giocare pari o dispari?");
+let numeroInserito=Number(prompt("Inserisci un numero da 1 a 5"));
+console.log(numeroInserito);
+let min=1;
+let max=5;
+
+function getRandomInt(min, max) {
+    min = Math.ceil();
+    max = Math.floor();
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  let somma=numeroInserito+Math.floor(Math.random() * (max - min) + min);;
+  console.log(somma);
+
+  function numeroPariDispari(somma){
+    if(somma % 2==0){
+        return"pari"
+    }else{
+       
+        return "dispari";
+    }
+} console.log(numeroPariDispari(somma))
+
+if(numeroPariDispari(pariDispari)=="pari"){
+    document.getElementById("numero").innerHTML=`${pariDispari}L'utente ha vinto`;
+}else{
+    document.getElementById("numero").innerHTML=` L'utente ha perso`;
+}
